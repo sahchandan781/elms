913fb54f9be551ab171056345c737cae0038d9d1
Chandan kumar Sah <93442672+sahchandan781@users.noreply.github.com>
2025-04-18 15:53:46 +0530

    add Update course api

--- backend/controllers/course.controller.js
+++ backend/controllers/course.controller.js
@@ -1,4 +1,5 @@
 import { Course } from "../models/course.model.js";
+import {deleteMediaFromCloudinary, uploadMedia} from "../utils/cloudinary.js"
 
 export const createCourse = async (req, res) =>{
     try {
@@ -41,6 +42,49 @@ export const getcreatorCourses = async (req, res) => {
         return res.status(200).json({
             courses,
         })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message:"Failed to fetch course!"
+        })
+    }
+}
+
+
+export const editCourse = async(req, res) =>{
+    try {
+        const courseId = req.params.courseId;
+        const {courseTitle, subTitle, description, category, courseLevel, coursePrice} = req.body;
+        const thumbnail = req.file;
+
+        let course = await Course.findById(courseId)
+        if(!course){
+            return res.status(404),json({
+                message: "Course not found!"
+            })
+        }
+
+        let courseThumbnail;
+        if(thumbnail){
+            if(course.courseThumbnail){
+                const publicId = course.courseThumbnail.split("/").pop().split(".")[0];
+                await deleteMediaFromCloudinary(publicId); // delete old image
+            }
+            // upload new thumbnail on cloudinary
+            
+            courseThumbnail = await uploadMedia(thumbnail.path)
+        }
+
+
+
+        const updateData = {courseTitle, subTitle, description, category, courseLevel, coursePrice, courseThumbnail: courseThumbnail?.secure_url }
+        course = await Course.findByIdAndUpdate(courseId, updateData, {new:true});
+
+        return res.status(200).json({
+            course,
+            message: "Course updated successfully"
+        })
+
     } catch (error) {
         console.log(error);
         return res.status(500).json({
--- backend/routes/course.route.js
+++ backend/routes/course.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import isAuthenticated from "../middlewares/isAuthenticated.js"
-import { createCourse, getcreatorCourses } from "../controllers/course.controller.js"
+import { createCourse, editCourse, getcreatorCourses } from "../controllers/course.controller.js"
+import upload from "../utils/multer.js"
 
 const router = express.Router();
 
 router.route("/").post(isAuthenticated, createCourse);
 router.route("/").get(isAuthenticated, getcreatorCourses);
+router.route("/").put(isAuthenticated,upload.single("courseThumbnail"), editCourse);
 
 
 export default router;
